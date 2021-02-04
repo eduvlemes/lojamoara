7c715e08dd4d11b1d4d57ebd0eea6a17910d8ab8
eduvlemes <edu.vlemes@gmail.com>
2021-02-04 16:32:18 -0300

    Moara ajustes home 4

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,10 +1,12 @@
 // Add your custom JavaScript for storefront pages here.
 
-$(document).ready(function(){
+$(window).resize(function(){
     let w = $('.product-card__pictures').first().innerWidth();
     let h = 1.4;
     console.log((h*w) + 'px');
-    $('.apx .product-card__pictures').css('height', (h*w) + 'px');
+    $('.apx .product-card__pictures').css('height', (h*w) + 'px');    
+});
+$(document).ready(function(){    
     $('.apx-product_home_list').addClass('normal');
     if(apx_bannersPage && $('.sections>.collection-shelf:nth-child(2) .apx-product_home_list').length > 0){        
         const list = '.sections>.collection-shelf:nth-child(2) .apx-product_home_list';
