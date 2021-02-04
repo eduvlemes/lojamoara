88eea01709ae462c84e5ce81fd3bf7d20d6ffabe
eduvlemes <edu.vlemes@gmail.com>
2021-02-04 00:32:20 -0300

    Tema Moara

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,6 @@
 // Add your custom JavaScript for storefront pages here.
+$(document).ready(function(){
+    let w = $('.product-card__pictures').first().innerWidth();
+    let h = 1.4;
+    $('.product-card__pictures').css('height', (h*w) + 'px');
+});
