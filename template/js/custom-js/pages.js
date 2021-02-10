ffcd8c23946a209940b92ae9c58f8312d436f2fe
eduvlemes <edu.vlemes@gmail.com>
2021-02-10 16:48:50 -0300

    slider

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -10,33 +10,33 @@ function ajustaImagens(){
     }) 
 };
 $(document).ready(function(){    
-    $('.apx-product_home_list').addClass('normal');
-    if(apx_bannersPage && $('.sections>.collection-shelf:nth-child(2) .apx-product_home_list').length > 0){        
-        const list = '.sections>.collection-shelf:nth-child(2) .apx-product_home_list';
-        $(list).removeClass('normal');
-        $('<li><div class="banner"> <a href="'+ apx_bannersPage.url_s + '" > <picture class="img-fluid" data-iesrc="'+ apx_bannersPage.image_s_d.replace('.webp', '.png') + '" > <source srcset="'+ apx_bannersPage.image_s_d.replace('.webp', '.png') +'" media="(min-width: 576px)"> <source srcset="'+ apx_bannersPage.image_s_m.replace('.webp', '.png') +'" media="(min-width: 100px)"> <img src="'+ apx_bannersPage.image_s_d.replace('.webp', '.png') +'"> </picture> </a> </div></li>').prependTo(list);
-        $(list).addClass('withBanner');
-    }
+    // $('.apx-product_home_list').addClass('normal');
+    // if(apx_bannersPage && $('.sections>.collection-shelf:nth-child(2) .apx-product_home_list').length > 0){        
+    //     const list = '.sections>.collection-shelf:nth-child(2) .apx-product_home_list';
+    //     $(list).removeClass('normal');
+    //     $('<li><div class="banner"> <a href="'+ apx_bannersPage.url_s + '" > <picture class="img-fluid" data-iesrc="'+ apx_bannersPage.image_s_d.replace('.webp', '.png') + '" > <source srcset="'+ apx_bannersPage.image_s_d.replace('.webp', '.png') +'" media="(min-width: 576px)"> <source srcset="'+ apx_bannersPage.image_s_m.replace('.webp', '.png') +'" media="(min-width: 100px)"> <img src="'+ apx_bannersPage.image_s_d.replace('.webp', '.png') +'"> </picture> </a> </div></li>').prependTo(list);
+    //     $(list).addClass('withBanner');
+    // }
 
     $('.custom-html .divider').closest('section').addClass('break-page');
 
-    if($('#instafeed').length){
-        $('#instafeed').closest('.custom-html').addClass('instafeed-container');
-        var obj = new InstagramFeed({
-            'username': 'moaraacessorios',
-            'container': document.getElementById("instafeed"),
-            'display_profile': false,
-            'display_biography': false,
-            'display_gallery': true,
-            'display_captions': false,
-            'display_igtv': false,
-            'get_raw_json': false,
-            'lazy_load':false,
-            'callback': null,
-            'styling': false,
-            'items': 4,
-            'items_per_row': 4,
-            'margin': 1
-        });
-    }
+    // if($('#instafeed').length){
+    //     $('#instafeed').closest('.custom-html').addClass('instafeed-container');
+    //     var obj = new InstagramFeed({
+    //         'username': 'ecomclub',
+    //         'container': document.getElementById("instafeed"),
+    //         'display_profile': false,
+    //         'display_biography': false,
+    //         'display_gallery': true,
+    //         'display_captions': false,
+    //         'display_igtv': false,
+    //         'get_raw_json': false,
+    //         'lazy_load':false,
+    //         'callback': null,
+    //         'styling': false,
+    //         'items': 4,
+    //         'items_per_row': 4,
+    //         'margin': 1
+    //     });
+    // }
 });
